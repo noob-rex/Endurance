93ae6b8ed8481d4e70fd68f664ff031d23c18d0a
noob-rex <prabin01284939@gmail.com>
2022-07-09 15:47:05 +0530

    a react workout app

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner.png';
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#F37878" fontWeight="600" fontSize="30px">Fitness Club</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Sweat, Smile <br />
+      And Repeat
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="36px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#F37878" sx={{ opacity: '0.4', display: { lg: 'initial', xs: 'none' }, fontSize: '250px' }}>
+      EXERCISES
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+
+  </Box>
+);
+
+export default HeroBanner;
